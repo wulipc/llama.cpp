45fd5126a7976b1825453c74a2fd2fd7e2d1b692
Johannes Gäßler <johannesg@5d6.de>
2024-11-17 14:58:51 +0100

    llama/ggml: add LLM training support
    more compact progress bar
    
    refactor: llama_prepare_sbatch/ubatch
    
    llama_save_model_to_file
    
    gqa_mode arg for repeat_back
    
    llama_opt_param_filter
    
    ggml_graph_dup force_grads
    
    refactor ggml_opt, fix test-opt

--- src/llama-model.h
+++ src/llama-model.h
@@ -90,6 +90,8 @@ enum llm_type {
     LLM_TYPE_17B_128E, // llama4 Maverick
 };
 
+std::string llama_rope_scaling_type_name(llama_rope_scaling_type rope_scaling_type);
+
 struct llama_layer_posnet {
     // resnet
     struct ggml_tensor * norm1   = nullptr;
